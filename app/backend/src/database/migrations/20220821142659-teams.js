e0e376a579237676a93b1908828414d7c63d60e0
Ramos <beidseitig@gmail.com>
2022-08-21 13:06:31 -0300

    feat: req14/18 - configured migrations of teams and matches

--- app/backend/src/database/migrations/20220821142659-teams.js
+++ app/backend/src/database/migrations/20220821142659-teams.js
@@ -1,21 +1,20 @@
-'use strict';
-
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('teams', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      teamName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        field: 'team_name',
+      }
+    })
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+    await queryInterface.dropTable('teams');
+  },
 };
--- app/backend/src/database/migrations/20220821142714-matches.js
+++ app/backend/src/database/migrations/20220821142714-matches.js
@@ -1,21 +1,49 @@
-'use strict';
-
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('matches', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      homeTeam: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: 'home_team',
+        references: {
+          model: 'Team',
+          key: 'id',
+        },
+      },
+      homeTeamGoals: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        field: 'home_team_goals',
+      },
+      awayTeam: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        field: 'away_team',
+        references: {
+          model: 'Team',
+          key: 'id',
+
+        }
+      },
+      awayTeamGoals: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        field: 'away_team_goals',
+      },
+      inProgress: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        field: 'in_progress',
+      }
+    })
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+    await queryInterface.dropTable('matches');
+  },
 };
